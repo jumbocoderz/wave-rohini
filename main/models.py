70406ca6d907ed611ddb75a9e359f6dcb7c7f67a
jumbocoderz <imvikas2016jha@gmail.com>
2020-04-22 22:57:11 +0530

    Update activation URL from local host to allowed host

--- main/models.py
+++ main/models.py
@@ -33,7 +33,7 @@ class EmailConfirmed(models.Model):
         return str(self.confirmed)
 
     def activate_user_email(self):
-        activation_url = "http://localhost:8000/main/activate/%s" %(self.activation_key)
+        activation_url = "https://wave-rohini.herokuapp.com/main/activate/%s" %(self.activation_key)
         context = {
             "activation_key" : self.activation_key,
             "activation_url" : activation_url,
@@ -42,7 +42,7 @@ class EmailConfirmed(models.Model):
         message = render_to_string("main/activation_message.txt",context)
         subject = "Activate your email"
         # print(message)
-        self.email_user(subject, message, settings.DEFAULT_FROM_EMAIL)
+        self.email_user(subject, message, settings.EMAIL_HOST_USER)
 
     def email_user(self, subject, message, from_email=None, **kwargs):
         send_mail(subject, message, from_email, [self.user.email], kwargs)
